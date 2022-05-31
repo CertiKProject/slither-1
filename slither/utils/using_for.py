402b418ce6e9130507c5d32941cf853882147b12
Runming Li <runming.li@certik.com>
2022-05-31 16:34:59 -0400

    refactor: minor update from Structure to StructureContract for convenience (#2)

--- slither/utils/using_for.py
+++ slither/utils/using_for.py
@@ -1,12 +1,12 @@
-from typing import Dict, List, TYPE_CHECKING, Union
+from typing import Dict, List, Tuple, TYPE_CHECKING, Union
 from slither.core.solidity_types import Type, UserDefinedType
 
 if TYPE_CHECKING:
-    from slither.core.declarations import Function, Structure
+    from slither.core.declarations import Function, StructureContract
 
 USING_FOR_KEY = Union[str, Type]  # "*" is wildcard
 USING_FOR_ITEM = List[Union[UserDefinedType, "Function"]]  # UserDefinedType.type is a library
-USING_FOR_SRC_ITEM = List[Tuple[Union[UserDefinedType, "Function"], "Structure"]]  # UserDefinedType.type is a library
+USING_FOR_SRC_ITEM = List[Tuple[Union[UserDefinedType, "Function"], "StructureContract"]]  # UserDefinedType.type is a library
 USING_FOR = Dict[USING_FOR_KEY, USING_FOR_ITEM]
 USING_FOR_SRC = Dict[USING_FOR_KEY, USING_FOR_SRC_ITEM]
 
