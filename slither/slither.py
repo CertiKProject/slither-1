CertiKProject/slither-certik

feat: add constant folding to IR (#30)
https://github.com/CertiKProject/slither-task/issues/327

* feat: add constant folding to IR
* added certik ir flag to SlitherCompilationUnit

slither/slither.py
@@ -106,12 +106,12 @@ class Slither(SlitherCore):  # pylint: disable=too-many-instance-attributes
             raise SlitherError(f"Invalid compilation: \n{str(e)}")
 
         for compilation_unit in crytic_compile.compilation_units.values():
-            compilation_unit_slither = SlitherCompilationUnit(self, compilation_unit)
+            compilation_unit_slither = SlitherCompilationUnit(self, compilation_unit, generates_certik_ir = False)
             self._compilation_units.append(compilation_unit_slither)
             parser = SlitherCompilationUnitSolc(compilation_unit_slither, generates_certik_ir = False)
             self._parsers.append(parser)
 
-            new_compilation_unit_slither = SlitherCompilationUnit(self, compilation_unit)
+            new_compilation_unit_slither = SlitherCompilationUnit(self, compilation_unit, generates_certik_ir = True)
             self._certik_compilation_units.append(new_compilation_unit_slither)
             new_parser = SlitherCompilationUnitSolc(new_compilation_unit_slither, generates_certik_ir = True)
             self._parsers.append(new_parser)

CertiKProject/slither-1

slither/slither.py
@@ -139,9 +139,9 @@ class Slither(
             raise SlitherError(f"Invalid compilation: \n{str(e)}")
 
         for compilation_unit in crytic_compile.compilation_units.values():
-            compilation_unit_slither = SlitherCompilationUnit(self, compilation_unit)
+            compilation_unit_slither = SlitherCompilationUnit(self, compilation_unit, generates_certik_ir = False)
             self._compilation_units.append(compilation_unit_slither)
-            certik_compilation_unit_slither = SlitherCompilationUnit(self, compilation_unit)
+            certik_compilation_unit_slither = SlitherCompilationUnit(self, compilation_unit, generates_certik_ir = True)
             self._certik_compilation_units.append(certik_compilation_unit_slither)
 
             if compilation_unit_slither.is_vyper: