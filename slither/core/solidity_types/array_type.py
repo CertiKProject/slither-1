CertiKProject/slither-certik

fix: removed equality method from Literal class (#57)
- updated ArrayType's equality check

https://github.com/CertiKProject/slither-task/issues/674

slither/core/solidity_types/array_type.py
@@ -74,7 +74,18 @@ class ArrayType(Type):
     def __eq__(self, other: Any) -> bool:
         if not isinstance(other, ArrayType):
             return False
-        return self._type == other.type and self.length == other.length
+        return (
+            self._type == other.type
+            and (
+                (
+                    isinstance(self.length, Literal)
+                    and isinstance(other.length, Literal)
+                    and self.length.value == other.length.value
+                    and self.length.subdenomination == other.length.subdenomination
+                )
+                or self.length == other.length
+            )
+        )
 
     def __hash__(self) -> int:
         return hash(str(self))

CertiKProject/slither-1

slither/core/solidity_types/array_type.py
@@ -74,7 +74,16 @@ class ArrayType(Type):
     def __eq__(self, other: Any) -> bool:
         if not isinstance(other, ArrayType):
             return False
-        return self._type == other.type and self._length_value == other.length_value
+        return (
+            self._type == other.type
+            and (
+                (
+                    self.length_value.value == other.length_value.value
+                    and self.length_value.subdenomination == other.length_value.subdenomination
+                )
+                or self.length_value == other.length_value
+            )
+        )
 
     def __hash__(self) -> int:
         return hash(str(self))